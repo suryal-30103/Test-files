7bf5817c85eb633b1d3d44defaffda476c2cd7d7
L SURYA <94703794+suryal-30103@users.noreply.github.com>
2022-02-04 19:00:44 +0530

    Update MINI PROJECT.py

--- MINI PROJECT.py
+++ MINI PROJECT.py
@@ -1,51 +1,96 @@
-state_dict= { "andhra pradesh": "Tirupati , Vizag , Vijaywada , Amravathi , Kakinada , Rajahmundry , Chitoor , Kadapa , Nellore , Rajole , Gudur",
-"arunachal pradesh": "Tawang , Zero , Itanagar , Bomdila , Pasighat", 
-"assam": "Guwahati , Silchar , Dibrugarh , Jorhat , Nagaon", 
-"bihar": "Patna , Gaya , Bhagalpur , Muzzafarpur , Dharbhanga",
-"chattisgarh": "Raipur , Bilaspur , Durg , Janjgir-Champa , Rajnandgaon ",
-"goa": "Panaji , Madgaon , Mormugao , Mapuca , Ponda",
-"gujurat" : "Gandhi nagar , Ahmedabad , Surat , Vadodara , Rajkot , Bhavnagar",
-"haryana" : "Faridabad , Gurugram , Chandigarh , Panipat , Ambala , Yamunanagar",
-"himachal pradesh" : "Shimla , Solan , Kullu, Baddi , Mandi",
-"jharkand" : "Jamshedpur , DhanbadRanchi , Bokaro Steel City , Deoghar",
-"karnataka" : "Bengaluru, Hubballi-Dharwad , Mysuru , Davangere , Mangaluru, Tumkur, Chikkamagaluru ",
-"kerala" : "Thiruvananthapuram, Kozhikode, Kochi, Kollam, Thrissur, Kannur",
-"madhya pradesh" : "Indore, Bhopal, Jabalpur, Gwalior, Ujjain, Sagar",
-"maharashtra" : "Mumbai, Pune, Nagpur, Thane, Nashik, Amravati, Nashik,",
-"manipur": "Imphal, Imphal, Bishnupur, Senapati, Chandel, Tamenglong, Ukhrul",
-"meghalaya": "Shillong, Cherrapunji, Guwahait, Tura, Baghamra, Nongstoin, Garo hills, ",
-"mizoram": "Aizawl, Champhai, Kolasib, Lawngtlai, Lunglei, Mamit, Saiha, Serchhip",
-"nagaland":"Dimapur, Kohima, Soma, Tuensang",
-"odisha" : "Bhubaneswar, Cuttack, Rourkela, Berhampur, Sambalpur, Puri, Balasore",
-"punjab": "Amritsar, Ludhiana, Jalandhar, Patiala, Bathinda, ,Mohali",
-"rajasthan": "Jaipur, Ajmer, Jodhpur, Kota, Bhiwadi, Bikaner,Udaipur",
-"sikkim": " Gangtok, Pelling, Lachung, Lachung, Yuksom, ",
-"tamil nadu": "Chennai, Coimbatore, Madurai, Tiruchirappalli, Salem, Tirunelveli, Tiruppur, Vellore, Thoothukkudi",
-"telangana": "Hyderabad, Warangal, Nizamabad, Khammam, Karimnagar, Peddapalli, Suryapet",
-"tripura": "Agartala, Dharmanagar, Udaipur, Kailashahar, Bishalgarh, ",
-"uttar pradesh": "Kanpur, Lucknow, Ghaziabad, Agra, Meerut, Varanasini, Prayagraj",
-"uttarakhand": "Dehradun, Haridwar, Roorkee, Rudrapur, Kashipur",
-"west bengal": "Kolkata , Asansol, Siliguri, Durgapur, Bardhaman, Malda, Baharampur"}
+'''                ██████████████████████████████████████████████████                
+           ████████████████▓▓██████████▓▓▓▓████████████▓▓██████████████▒▒          
+         ████████████████████▓▓████████▓▓▓▓████████████████████████████████        
+       ██████████████████████▓▓████████▒▒▓▓██████████████████████████████████      
+     ██████████████████████████████████▒▒▒▒▓▓████▓▓████████████████████████████    
+     ████████████████████████████████░░░░░░▒▒▓▓████████████████████████████████▓▓  
+   ▒▒████████████████████████████▒▒▒▒░░▒▒░░░░░░▒▒▓▓██████████████████████████████  
+   ████████████████████████████▒▒░░░░░░░░░░░░░░░░░░▓▓████████████████████████████▓▓
+   ██████████████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓████████████████████████████
+ ░██████████████████████████░░▒▒▒▒▒▒░░░░▒▒░░░░▒▒░░░░▒▒████████████████████████████
+ ░████████████████████████▒▒░░▒▒░░░░░░░░░░░░░░░░░░░░░░▓▓██████████████████████████
+ ░████████████████████████▓▓░░▒▒░░░░░░░░░░░░░░░░░░░░▒▒▓▓██████████████████████████
+  ░████▒▒▓▓██████████████▒▒▒▒▒▒░░░░▒▒░░▒▒░░░░▒▒▒▒░░▒▒▒▒▒▒▓▓██████████████▒▒▒▒██▓▓▓▓
+   ██▓▓▓▓▒▒████████████▒▒░░░░░░▒▒░░░░░░░░░░░░░░░░░░▒▒░░░░░░▓▓████████████▒▒▒▒▓▓▓▓██
+ ░░████░░▓▓██▒▒▓▓██████▓▓▒▒▓▓▒▒▒▒░░▒▒▒▒▒▒░░░░▒▒▒▒░░▒▒▒▒▓▓▒▒████████▓▓▓▓██▒▒▒▒▓▓████
+   ████▒▒▓▓██▓▓▓▓██▓▓▓▓▓▓▒▒▓▓▒▒▒▒░░░░░░▒▒░░░░░░▒▒░░▒▒▒▒▓▓▒▒▓▓▓▓▓▓██▓▓████░░▒▒▓▓████
+ ░░████▒▒▓▓██▒▒▓▓██▒▒▒▒▒▒▒▒▓▓░░▒▒░░░░▒▒████░░░░░░░░░░▒▒██░░▒▒▒▒▓▓██▒▒▓▓██▒▒▒▒▓▓████
+ ░░████▒▒▓▓██░░▓▓██▒▒▓▓▒▒▓▓██▒▒▒▒░░▒▒████████▒▒▒▒░░░░▓▓██░░██▒▒▓▓██▒▒▓▓██▒▒▒▒▓▓████
+ ░░██▒▒▒▒▒▒▓▓▒▒▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▓▓██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██░░▓▓██▒▒▒▒▒▒████
+ ░░██▓▓░░▒▒▓▓▒▒▒▒██▒▒▓▓▒▒▓▓██░░▒▒░░▒▒████████▒▒░░░░░░▓▓██░░▓▓▒▒▓▓██░░▒▒██▒▒░░▓▓████
+ ░░██▓▓░░▒▒▓▓▒▒▒▒██▒▒▓▓▒▒▓▓██░░▒▒░░▒▒████████▒▒░░░░░░▓▓██░░▓▓▒▒▓▓██░░▒▒██▒▒░░▓▓████
+ ░░██▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▓▓██
+ ░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██
+   ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██
+ ░░████████████████████████████████████████████████████▓▓██████████████████████████
+ ░░████▓▓██████████████████      ██  ██████  ██        ████      ████      ████████
+   ████████              ████  ████    ████  ██  ██████  ████  ████  ██████  ██   ██
+ ░░▓▓███                 ████  ████  ██  ██  ██  ██████  ████  ████          ██████
+   ██████▒▒▒▒██▓▓▓▓██▒▒▓▓████  ████  ████    ██  ██████  ████  ████  ██████  ██████
+     ▒▒████████████████████      ██  ██████  ██        ████      ██  ██████  ██████
+       ████████████████████████████████████████████████████████████████████████████'''
 
-print("Welcome to know important cities of all states in India")
+ 
+
+
+state_dict= { 'andhra pradesh': {'CAPITAL':'Amaravati' , 'PILIGRIMAGE':'Tirupati, Srisailam, Ahobilam , Vijayawada',  'METROPOLITAN CITIES':'Amaravati, Rajamendravaram, Guntur, Nellore, Kurnool' , 'HIGHLY POPULATED CITY':'Vishakapatnam' },
+"arunachal pradesh":{'CAPITAL': 'Itanagar', 'PILIGRIMAGE':'Urgelling Monastery,Tawang Monestry, Chillipam Gompa, Golden Pagoda ' , 'METROPOLITAN CITIES':'Itanagar, Dibang Valley, West Siang, West Kameng, Upper Subansiri' , 'HIGHLY POPULATED CITY':'Itanagar' },
+"assam":{'CAPITAL':'Dispur','PILIGRIMAGE':'Kamakhya Temple,' ,'METROPOLITAN CITIES':'Dispur, Guwahati, Silchar, Dibrugarh','HIGHLY POPULATED CITY':'Guwahati'},
+"bihar": {'CAPITAL':'Patna' , 'PILIGRIMAGE ':'Gaya, Mahavira Temple, Rajgir, Vishwa Shanti Stupa' , 'METROPOLITAN CITIES':'Gaya, Bhagalpur, Muzzafarpur, Darbhanga' , 'HIGHLY POPULATED CITY':'Patna' },
+"chattisgarh": {'CAPITAL':'Raipur', 'PILIGRIMAGE':'Danteshwari Temple, Barsur Twin Ganesha temple,Maa Baleshwari temple' , 'METROPOLITAN CITIES':'Bhilai, Bilaspur, Korba, Rajnandgaon' , 'HIGHLY POPULATED CITY':'Naya Raipur' },
+"goa": {'CAPITAL':'Panaji', 'PILIGRIMAGE':'Velha-Old Goa, Shri Mangesha Temple, Se Cathedral' , 'METROPOLITAN CITIES':'Panaji, Vaso da game, Margao, MApusa, Ponda' , 'HIGHLY POPULATED CITY':'Panaji' },
+"gujurat" : {'CAPITAL':'Gandhinagar', 'PILIGRIMAGE':'Veraval,Dwarka,Somanath, Swaminarayana Akshardham Temple' , 'METROPOLITAN CITIES':'ahmedabad, Surat, Rajkot, Vadodara,' , 'HIGHLY POPULATED CITY':'Ahmedabad' },
+"haryana" : {'CAPITAL': 'Chandigarh', 'PILIGRIMAGE':'Gurudwara Nada Sahib,Mata Mansa Devi Temple,Kartikeya temple' , 'METROPOLITAN CITIES':'Chandigarh,Faridabad,Gurugram,Panipat,Ambala' , 'HIGHLY POPULATED CITY':'Faridabad' },
+"himachal pradesh" : {'CAPITAL':'Shimla', 'PILIGRIMAGE':'Naina Devi,Jwala Mukhi Temple,Baijnath,Haimba Devi' , 'METROPOLITAN CITIES':'Shimla,Dharamsala,Solan,Mandi' , 'HIGHLY POPULATED CITY':'Shimla' },
+"jharkand" : {'CAPITAL':'Ranchi' , 'PILIGRIMAGE':'Bhadra Kali Temple, Trikuti,Rajrappa,Jagganthpur Temple,Angrabari' , 'METROPOLITAN CITIES':'Ranchi,Jamshedpur,Dhanbad,Bokaro Steel City' , 'HIGHLY POPULATED CITY':'Jamshedpur' },
+"karnataka" : {'CAPITAL':'Bangalore' , 'PILIGRIMAGE':'Mahabaleshwar Temple ,Dharmastala,Srirangapatna,Humpi' , 'METROPOLITAN CITIES':'Bangalore,Chikmangaluru,Mysuru,Mangaluru,Belagavi' , 'HIGHLY POPULATED CITY':'Bengaluru' },
+"kerala" : {'CAPITAL':'Thiruvananthapuram' , 'PILIGRIMAGE':'Shabrimala ,Thrissur,Kottayam,Thrissur,Pathanamthitta' , 'METROPOLITAN CITIES':'Thiruvananthapuram,Kozhikode,Kochi,MArradu,Thrikkakara' , 'HIGHLY POPULATED CITY':'Thiruvananthapuram' },
+"madhya pradesh" :{'CAPITAL':'Bhopal' , 'PILIGRIMAGE':'Bhopal,Ujjain,Orccha,Maheshwar And Omkareshwar' , 'METROPOLITAN CITIES':'Bhopal,Indore,Jabalpur,Gwalior' , 'HIGHLY POPULATED CITY':'Indore' },
+"maharashtra" : {'CAPITAL':'Mumbai' , 'PILIGRIMAGE':'Shirdi,Jejuri,Nashik,Kohlapur' , 'METROPOLITAN CITIES':'Mumbai,Pune,Nagpur' , 'HIGHLY POPULATED CITY':'Mumbai' },
+"manipur": {'CAPITAL':'Imphal' , 'PILIGRIMAGE':'Sridhara Temple ,Radha Syam Temple,Shri Govindhajee Temple,Shri Hanuman Thakur,Kaina Hillock' , 'METROPOLITAN CITIES':'Imphal,Thoubal,Bishnupur,Senapati,Chandel' , 'HIGHLY POPULATED CITY':'Imphal' },
+"meghalaya": {'CAPITAL':'Shillong ', 'PILIGRIMAGE':'Kamakya Temple ,Mawjymbuin Cave Temple,MAwsynram,' , 'METROPOLITAN CITIES':'Shillong,Cherrapunjee,Tura,Nongpoh,Nongstoin' , 'HIGHLY POPULATED CITY':'Shillong' },
+"mizoram": {'CAPITAL':'Aizawl' , 'PILIGRIMAGE':'Vantawng Falls,Sarvajanin Temple,A.R Mandir' ,'METROPOLITAN CITIES':'Aizawl,Champai,Lunglei' , 'HIGHLY POPULATED CITY':'Aizawl' },
+"nagaland":{'CAPITAL':'Kohima' , 'PILIGRIMAGE':'Molunyimsen Christioan Heritage village,Molungkimong Baptist church', 'METROPOLITAN CITIES':'Kohima,Dimapur,Soma' , 'HIGHLY POPULATED CITY':'Dimapur' },
+"odisha" : {'CAPITAL':'Bhubaneswar' , 'PILIGRIMAGE':'Konark Sun temple, Bramheshwar temple,Puri Jaggnath Temple,Mukteshwar Temple,Rajarani Temple,Vital Deu temple', 'METROPOLITAN CITIES':'Bhubaneswar' , 'HIGHLY POPULATED CITY':'Bhubaneswar'},
+"punjab": {'CAPITAL':'Chandigarh' , 'PILIGRIMAGE':'Amritsar Golden temple' , 'METROPOLITAN CITIES':'Ludhiana,Amritsar' , 'HIGHLY POPULATED CITY':'Ludhiana' },
+"rajasthan": {'CAPITAL':'Jaipur' , 'PILIGRIMAGE':'Ajmer,Karni Mata Temple,Brahma Temple,Jain Temple' , 'METROPOLITAN CITIES':'Jaipur,Jodhpur,Kota' , 'HIGHLY POPULATED CITY':'Jaipur' },
+"sikkim": {'CAPITAL':'Gangtok' , 'PILIGRIMAGE':'Solophok Chardham,Kiratehwar Mahadev Temple' , 'METROPOLITAN CITIES':'Gangtok,Namchi,Chungthang,Dzuluk,Kaluk' , 'HIGHLY POPULATED CITY':'Gangtok' },
+"tamil nadu": {'CAPITAL':'Chennai' , 'PILIGRIMAGE':'Rameshwaram, Kanchipuram,Madurai,Vaitheeswaran Koil,Velankanni,Anubhavi Subramaniar Temple ', 'METROPOLITAN CITIES':'Chennai,Coimbatur,MAdurai,Salem,Tiruchirapalli' , 'HIGHLY POPULATED CITY':'Chennai' },
+"telangana": {'CAPITAL':'Hyderabad' , 'PILIGRIMAGE':'Yadigirigutta,Bhadrachalam,Basara,Vemulawada,Alampur', 'METROPOLITAN CITIES':'Hyderabad,' , 'HIGHLY POPULATED CITY':'Hyderabad '},
+"tripura": {'CAPITAL':'Agartala' , 'PILIGRIMAGE':'The fourteen Goddess temple,' , 'METROPOLITAN CITIES':'Agartala,Dharmanagar,Udaipur,Kaileshwar' , 'HIGHLY POPULATED CITY':'Agartala' },
+"uttar pradesh":{'CAPITAL':'Lucknow' , 'PILIGRIMAGE':'Varanasi,Sarnath,Mathura,Sravasthi ', 'METROPOLITAN CITIES':'Lucknow,Meerut,Agra,Varnasi', 'HIGHLY POPULATED CITY':'Lucknow' },
+"uttarakhand": {'CAPITAL':'Dehradun' , 'PILIGRIMAGE': 'Rishikesh, Badrinath,Haridwar,Kedarnath,Tarkeshwar Mahadev Temple' , 'METROPOLITAN CITIES':'Dehradun,Haridwar,Rishikesh' , 'HIGHLY POPULATED CITY':'Dehradun' },
+"west bengal": {'CAPITAL':'Kolkata' , 'PILIGRIMAGE':'Dakshineshwar kali temple,Belur Math,ISCKON Temple,Chinease Kali Temple' , 'METROPOLITAN CITIES':'Kolkata,Asansol,Chilguri,Durgapur' , 'HIGHLY POPULATED CITY':'Kolkata' }}
+
+
+print("/WELCOME TO KNOW IMPORATNAT CATEGORIES OF CITIES OF A STATE IN INDIA \
+       \******************************************************************* |")
 def state():
-    name=str(input("Enter state of india to know its important cities(in lower cases only):-"))
+    name=input("ENTER THE NAME OF THE STTE YOU WANT TO KNOW(in lower cases only)===>")
     if name in state_dict.keys():
-        print('Important cities of the state',name,'are--->',state_dict.get(name))
+        print('THE CATEGORIES OF CITIES WHICH CAN BE SHOWED ARE');print('CAPITAL');print('PILIGRIMAGE');print('METROPOLITAN CITIES');print('HIGHLY POPULATED CITY')
+        wh=input('WHICH CATEGORIES OF CITY IS REQUIRED FOR YOU?(Type as displayed previously)===>')
+        if wh in state_dict[name].keys():
+             print(state_dict[name][wh],"IS/ARE THE CITY/CITIES IN THE REQUIRED CATEGORY :)")
+        else:
+            print('OOPS!!!,GIVEN CATEGORY DOESNT EXIST IN OUR DATABASE!!!')
     
-        cont=input('Do you want to know for other states?(type Y/N):-')
-        if cont=='Y':
-            state()
-        elif cont=='N':
-            print('Thank you!!!')
-    else:
-        print('Given name does not exist')
+        cont=input('DO YOU WANT TO KNOW FOR OTHER STATE?(type Y/N):-')
         
-        cont=input('Do you want to know for other states?(type Y/N):-')
         if cont=='Y':
             state()
         elif cont=='N':
-            print('Thank you!!!')
+            print("\THANK YOU!!!\"") 
+                  
+    else:
+        print('GIVEN ANME OF STATE DOES NOT EXIST IN INDIA')
+        
+        cont=input('DO YOU WANT TO KNOW FOR OTHER STATE?(type Y/N):-')
+        if cont=='Y':
+            state()
+        elif cont=='N':
+            print("\THANK YOU!!!\"")
+                   
        
         
 state()
